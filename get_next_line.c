5b4f53d518f9b17fb5180762fd392f25fe06352b
kevseragdas <kevseragdas10@gmail.com>
2025-07-29 18:30:25 +0300

    ikinci değişim

--- get_next_line.c
+++ get_next_line.c
@@ -6,30 +6,33 @@
 /*   By: kagdas <kagdas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 13:33:39 by kagdas            #+#    #+#             */
-/*   Updated: 2025/07/27 16:12:46 by kagdas           ###   ########.fr       */
+/*   Updated: 2025/07/29 17:58:14 by kagdas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_clean_buff(char *buff)
+char	*ft_clean_buff(char **buff)
 {
 	char	*new_buff;
 	int		i;
 
 	i = 0;
-	while (buff[i] && buff[i] != '\n')
+	while ((*buff)[i] && (*buff)[i] != '\n')
 		i++;
-	if (!buff[i])
+	if (!(*buff)[i])
 	{
-		free(buff);
+		free(*buff);
+		*buff = NULL;
 		return (NULL);
 	}
 	i++;
-	new_buff = ft_substr(buff, i, ft_strlen(buff) - i);
-	free(buff);
+	new_buff = ft_substr(*buff, i, ft_strlen(*buff) - i);
+	free(*buff);
+	*buff = NULL;
 	return (new_buff);
 }
+
 
 static char	*ft_extract_line(char *buff)
 {
@@ -47,7 +50,7 @@ static char	*ft_extract_line(char *buff)
 	return (line);
 }
 
-static char	*ft_readfile(int fd, char *buff)
+static char	*ft_readfile(int fd, char **buff)
 {
 	char	*temp;
 	int		cntrl;
@@ -57,32 +60,34 @@ static char	*ft_readfile(int fd, char *buff)
 	if (!temp)
 		return (NULL);
 	cntrl = 1;
-	while (!ft_strchr(buff, '\n') && cntrl > 0)
+	while (!ft_strchr(*buff, '\n') && cntrl > 0)
 	{
 		cntrl = read(fd, temp, BUFFER_SIZE);
 		if (cntrl == -1)
-			return (free(temp), free(buff), NULL);
+			return (free(temp), free(*buff), NULL);
 		temp[cntrl] = '\0';
-		new = ft_strjoin(buff, temp);
+		new = ft_strjoin(*buff, temp);
 		if (!new)
 			return (free(temp), NULL);
-		free(buff);
-		buff = new;
+		free(*buff);
+		*buff = new;
 	}
 	free(temp);
-	return (buff);
+	return (*buff);
 }
+
 
 char	*get_next_line(int fd)
 {
 	static char	*buff;
 	char		*line;
+	char 		*new_buff;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	if (!buff)
 		buff = ft_calloc(1, 1);
-	buff = ft_readfile(fd, buff);
+	buff = ft_readfile(fd, &buff);
 	if (!buff || !*buff)
 	{
 		free(buff);
@@ -90,6 +95,7 @@ char	*get_next_line(int fd)
 		return (NULL);
 	}
 	line = ft_extract_line(buff);
-	buff = ft_clean_buff(buff);
+	new_buff = ft_clean_buff(&buff);
+	buff = new_buff;
 	return (line);
 }
